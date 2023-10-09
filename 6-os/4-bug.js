8e6167debcf03d3de2953daf3e0913e96a0dd9c3
aurelius.holgado <aurelius.holgado@pointwest.com.ph>
2023-10-09 16:17:17 +0800

    Completed until 6-os

--- 6-os/4-bug.js
+++ 6-os/4-bug.js
@@ -1,12 +1,19 @@
 function convertArrayToObject(arr) {
-  return arr.reduce((curr, acc) => {
-    acc[curr[0]] = curr[1];
+  return arr.reduce((acc, curr) => {
+    // acc is the empty array
+    // curr[0] is the key in the array, which are numbers
+    // curr[1] is the pair of the key
+    
+    console.log("acc: " + acc)
+    console.log("cur: " + curr)
+
+    acc[curr[0]] = curr[1]; 
     return acc;
   }, {});
 }
 
 const obj = convertArrayToObject([
-  [1, 'One'],
+  [1, 'One'], // acc[0] || 1 = acc[0][curr[0]] || 'One' = acc[0][curr[1]]
   [2, 'Two'],
   [3, 'Three'],
   [4, 'Four'],
@@ -25,3 +32,8 @@ console.log(obj);
   }
 
 */
+
+// Run this code in the terminal to inspect for bugs:
+  // node --inspect-brk 4-bug.js
+  // after which, go to chrome and open chrome://inspect
+  // no you have chrome dev tools for your node script

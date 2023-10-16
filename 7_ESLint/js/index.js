60ab1690a4c45c67cd3579178c9792352bacf3b6
aurelius.holgado <aurelius.holgado@pointwest.com.ph>
2023-10-16 15:28:36 +0800

    Added codes for add rules in ESLint

--- 7_ESLint/js/index.js
+++ 7_ESLint/js/index.js
@@ -1,6 +1,7 @@
 // eslint --init -> to install es lint and set up rules; you can see the requirements in .eslintrc.json
 // eslint folderName -> to run eslint on folder
 // eslint folderName --fix -> to automatically fix errors
+// https://eslint.org/docs/latest/rules/ check this link for other rules you may want to put
 
 let checkPositive = (number) => {
     if (number > 0) {

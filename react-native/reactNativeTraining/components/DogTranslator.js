607ac5d186f5b404a6e0f56925dfef004d5c9fb0
aurelius.holgado <aurelius.holgado@pointwest.com.ph>
2023-11-06 10:17:10 +0800

    Added theme responsiveness to dog translator

--- react-native/reactNativeTraining/components/DogTranslator.js
+++ react-native/reactNativeTraining/components/DogTranslator.js
@@ -1,19 +1,42 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, Appearance} from 'react-native';
 
 const DogTranslator = props => {
     const [text, setText] = useState("")
     const variations = ['Ow!', 'Arf!', 'Woof!', 'Bark!', 'Yip!'];
+    const theme = Appearance.getColorScheme();
     return(
         <View>
+            <Text
+                style={{
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                    margin: 10,
+                    textAlign: 'center'
+                }}>
+                Dog Translator
+            </Text>
+
             <TextInput
-                style={{height: 40}}
+                style={{
+                    height: 40,
+                    borderColor: theme === 'light' ? 'black' : 'white',
+                    borderWidth: 1,
+                    textAlign: 'center'
+                }}
                 placeholder="Type here to translate!"
+                placeholderTextColor={theme === 'light' ? 'black' : 'white'}
                 onChangeText={newText => setText(newText)}
                 defaultValue={text}
             />
 
-            <Text style={{padding: 10, fontSize: 42}}>
+            <Text
+                style={{
+                    padding: 10,
+                    fontSize: 24,
+                    textAlign: 'center'
+                }}>
+
                 {
                     text
                     .split(' ')

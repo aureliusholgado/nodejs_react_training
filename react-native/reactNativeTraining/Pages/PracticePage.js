dd0aafa80afba8fcca26b1a4f5aa24b6eae1701e
aurelius.holgado <aurelius.holgado@pointwest.com.ph>
2023-11-07 10:37:17 +0800

    Edited practice page

--- react-native/reactNativeTraining/Pages/PracticePage.js
+++ react-native/reactNativeTraining/Pages/PracticePage.js
@@ -1,22 +1,62 @@
 import React from 'react';
-import { Appearance, ScrollView, Text, StyleSheet, Button } from 'react-native';
+import { Appearance, ScrollView, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 const PracticePage = ({navigation}) => {
+    const theme = Appearance.getColorScheme();
+
+    // Background Paths
+    const lightBG = require('../images/lightWP.jpg')
+    const darkBG = require('../images/darkWP.jpg')
+
     const styles = StyleSheet.create({
         text:{
-            color:'black',
-            fontSize: 20
+            color: theme === 'light' ? 'white' : 'black',
+            backgroundColor: theme === 'light' ? 'orange' : 'white',
+            borderRadius: 10,
+            borderColor:'black',
+            borderWidth:5,
+            padding: 5,
+            textAlign: 'center',
+            fontSize: 25
+        },
+        customButton: {
+            backgroundColor: theme === 'light' ? 'orange' : 'purple',
+            padding: 10,
+            borderRadius:10,
+            borderWidth: 3,
+            borderColor: theme === 'light' ? 'black' : 'white',
+            margin: 5
+        },
+        customButtonText: {
+            color: 'white'
         }
     })
 
     return(
+        <ImageBackground
+            source={theme==='light' ? lightBG : darkBG}
+            style={{flex:1}}
+        >
         <ScrollView contentContainerStyle={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={styles.text} > Practice Page </Text>
-            <Button
-                title='Go to Practice Page... again'
-                onPress = { () => navigation.navigate('Practice Page') }
-            />
+
+            <Text style={styles.text}> Practice Page </Text>
+
+            {/* Button 1 */}
+            <TouchableOpacity
+                style = {styles.customButton}
+                /* this will not do anything
+                    onPress = { () => navigation.navigate('Practice Page') }
+                */
+
+                // this will add a new route
+                onPress = { () => navigation.push('Practice Page') }
+            >
+                <Text style={styles.customButtonText}>Go to Practice Page... again</Text>
+            </TouchableOpacity>
+
         </ScrollView>
+
+        </ImageBackground>
     )
 }
 

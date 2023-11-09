e1bbcb2c717f9ddd8055dce72745a32ff95fe9dd
aurelius.holgado <aurelius.holgado@pointwest.com.ph>
2023-11-09 10:08:24 +0800

    Added touchableOpacity to login page

--- react-native/reactNativeMiniProj/Pages/LoginPage.js
+++ react-native/reactNativeMiniProj/Pages/LoginPage.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 
 const LoginPage = ({navigation}) => {
+
+    const styles = StyleSheet.create({
+        container:{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        },
+        text:{
+            color:'black'
+        }
+    })
+
     return (
-        <View>
-            <Text style={{color:'black'}}>Hello from Login Page</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello from Login Page</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Section')}>
+                <Text>Go to Section</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Carousel')}>
+                <Text>Go to Carousel</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
--- react-native/reactNativeMiniProj/Pages/SectionPage.js
+++ react-native/reactNativeMiniProj/Pages/SectionPage.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 const SectionPage = ({navigation}) => {
+
+    const styles = StyleSheet.create({
+        container:{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        },
+        text:{
+            color:'black'
+        }
+    })
+
     return (
-        <View>
-            <Text>Hello from Section Page</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello from Section Page</Text>
         </View>
     )
 }
